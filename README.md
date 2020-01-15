eeb03d188abf98dba864ab3d0bf956c37f494199
Seva Netrebchenko <seva.netrebchenko@gmail.com>
2020-01-14 18:41:06 -0800

    Initializing repo with sigma project

---
+++ README.md
@@ -0,0 +1,2 @@
+# svg-html-css
+Fun with SVG graphics on an HTML/CSS Canvas
---
+++ sigma/app.js
@@ -0,0 +1,5 @@
+const sigma = document.querySelectorAll("#sigma path");
+
+for (let i = 0; i < sigma.length; ++i) {
+    console.log('Length: ${sigma[i].getTotalLength()} ');
+}
